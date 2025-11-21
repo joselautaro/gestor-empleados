442ee1b8c23070835191bacc68f078bddf580b94
Lautaro Montaña <joselautarom@gmail.com>
2025-11-21 06:14:51 -0300

    Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="modal-backdrop">
-        
+    <div class="modal-backdrop" @click.self="close">
+        <div class="modal-window card">
+            <slot/>
+        </div>
     </div>
 </template>
+
+<script setup>
+    import { defineEmits } from 'vue';
+
+    const emit = defineEmits(['close']);
+
+
+    function close(){
+        emit('close');
+    }
+</script>
+
+<style scoped>
+    .modal-backdrop{
+        position: fixed;
+        inset: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: linear-gradient(180deg, rgba(2,6,23,0.45), rgba(2,6,23,0.6));
+        z-index: 2000;
+    }
+</style>
