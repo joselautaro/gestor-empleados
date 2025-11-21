067d6e21aa4000b7ece9e676c8b433de5e079aad
Lautaro Montaña <joselautarom@gmail.com>
2025-11-21 06:55:00 -0300

    Cambios en App

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,15 @@
 <template>
   <div class="container">
     <Header/>
+    <ConfirmModal>
+      <p>Esta acción eliminará permanentemente al empleado seleccionado.
+        ¿Desea continuar?
+      </p>
+    </ConfirmModal>
   </div>
   
 </template>
   <script setup>
     import Header from './components/Header.vue'
+    import ConfirmModal from './components/ConfirmModal.vue'
   </script>
